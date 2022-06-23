310795f8a1f1b0fe559f580b90adfc2bba595148
Rashindra yadav <yadavrashindra5@gmail.com>
2022-06-23 22:50:50 +0530

    arraylist

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,18 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Rashindra yadav");
+        ArrayList<String>list=new ArrayList<>();
+        list.add("Data structure using c");
+        list.add("Data Structure using Java");
+        list.add("Python Programming");
+        list.add("C++ programming");
+        list.add("Rest Api");
+        list.add("Spring boot");
+        list.add("Hibernate");
+        list.add("Spring");
+        list.add("Servlet");
+        list.add("Java server page");
+        System.out.println(list);
     }
 }
 
