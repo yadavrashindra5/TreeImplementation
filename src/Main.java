e724630c51e1691c583482018628799af827d2fa
Rashindra yadav <yadavrashindra5@gmail.com>
2022-06-24 00:44:44 +0530

    Node is created for single Node and createBTree is used to create binary tree

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String>list=new ArrayList<>();
-        list.add("Data structure using c");
-        list.add("Data Structure using Java");
-        list.add("Python Programming");
-        list.add("C++ programming");
-        list.add("Rest Api");
-        list.add("Spring boot");
-        list.add("Hibernate");
-        list.add("Spring");
-        list.add("Servlet");
-        list.add("Java server page");
-        System.out.println(list);
+
+        TreeMethods tree=new TreeMethods();
+        Node root=null;
+        root=tree.createBTree(root);
     }
 }
 
--- src/TreeMethods.java
+++ src/TreeMethods.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class TreeMethods {
+    /*
+    *
+    * @Param root
+    * This method is used to create binary tree
+    * */
+    public Node createBTree(Node root)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter data or enter -1 for null");
+        int data=scanner.nextInt();
+        if(data==-1)
+            return root;
+        root=new Node(data);
+        System.out.println("Enter left child of "+data);
+        root.left=createBTree(root.left);
+        System.out.println("Enter right child of "+data);
+        root.right=createBTree(root.right);
+        return root;
+    }
 }
