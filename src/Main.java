ab221784015df877fdf6a9d9161c8d7580a4aa15
Rashindra yadav <yadavrashindra5@gmail.com>
2022-06-23 22:57:36 +0530

    arraylist

--- src/Main.java
+++ src/Main.java
@@ -2,69 +2,13 @@ import java.util.*;
 
 public class Main {
 
-
-    public static int fact(int n)
-    {
-        if(n==1)
-            return 1;
-        int subProblem=fact(n-1);
-
-        return n*subProblem;
-    }
-
-    public static int fibonacciSeries(int n)
-    {
-        if(n==0)
-            return 0;
-        if(n==1)
-        {
-            return 1;
-        }
-        int sum1=fibonacciSeries(n-1);
-        int sum2=fibonacciSeries(n-2);
-        return sum1+sum2;
-    }
-    public static void printNatural(int n)
-    {
-        if(n==0)
-            return;
-        printNatural(n-1);
-        System.out.print(n+"\t");
-        return;
-    }
-    public static void printNaturalDec(int n)
-    {
-        if(n==0)
-            return;
-        System.out.println(n);
-        printNaturalDec(n-1);
-    }
-
-    public static void printIncDesc(int n)
-    {
-        if(n==1)
-        {
-            System.out.println(n);
-            return;
-        }
-        System.out.println(n);
-        printIncDesc(n-1);
-        System.out.println(n);
-    }
-
-    public static int blength(int n)
-    {
-        if(n==1)
-            return 2;
-        if(n==2)
-            return 3;
-        return blength(n-1)+blength(n-2);
-    }
-
-
     public static void main(String[] args) {
-
-
+        ArrayList<Integer>list=new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(9);
+        list.add(4);
+        System.out.println(list);
     }
 }
 
