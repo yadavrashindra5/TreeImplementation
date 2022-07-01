07f973e7e32cedcf20ef4fe52bcb689e2f54017a
yadavrashindra5 <yadavrashindra5@gmail.com>
2022-07-01 21:24:43 +0530

    mirror image

--- src/BST.java
+++ src/BST.java
@@ -45,4 +45,129 @@ public class BST{
         inOrderTraversal(root.right);
     }
 
+    public boolean searchNode(Node root,int data)
+    {
+        if(root==null)
+            return false;
+        if(root.data==data)
+        {
+            System.out.println("data found");
+            return true;
+        }
+        if(data<root.data)
+        {
+            return searchNode(root.left,data);
+        }
+
+        return searchNode(root.right,data);
+    }
+
+    public Node deleteNode(Node root,int data)
+    {
+        if(data<root.data)
+            root.left=deleteNode(root.left,data);
+        else if(data>root.data)
+            root.right=deleteNode(root.right,data);
+        else if(root.left!=null && root.right!=null)
+        {
+            Node temp=findLargestNode(root.left);
+            root.data=temp.data;
+            root.left=deleteNode(root.left,temp.data);
+        }
+        else{
+            if(root.left==null && root.right==null)
+            {
+                return null;
+            }
+            else if(root.left!=null)
+            {
+                return root.left;
+            }
+            else
+                return root.right;
+        }
+        return root;
+    }
+
+
+    public Node findLargestNode(Node root)
+    {
+        if(root.left==null&&root.right==null)
+        {
+            return root;
+        }
+        else if(root.right==null)
+        {
+            return root;
+        }
+        return findLargestNode(root.right);
+    }
+
+    public Node findSmallestNode(Node root)
+    {
+        if(root.left==null && root.right==null)
+        {
+            return root;
+        }
+        else if(root.left==null)
+            return root;
+        return findSmallestNode(root.left);
+    }
+
+
+    public int height(Node root)
+    {
+        if(root==null)
+            return 0;
+
+        int lHeight=height(root.left);
+        int rHeight=height(root.right);
+
+        return lHeight>rHeight?lHeight+1:rHeight+1;
+
+    }
+
+    public int totalNode(Node root)
+    {
+        if(root==null)
+            return 0;
+        int left=totalNode(root.left);
+        int right=totalNode(root.right);
+        return left+right+1;
+    }
+
+    public int internalNode(Node root)
+    {
+        if(root==null)
+            return 0;
+        else if(root.left==null && root.right==null)
+            return 0;
+        return internalNode(root.left)+internalNode(root.right)+1;
+    }
+
+    public int externalNode(Node root)
+    {
+        if(root==null)
+            return 0;
+        else if(root.left==null && root.right==null)
+            return 1;
+        return externalNode(root.left)+externalNode(root.right);
+    }
+
+
+    public Node mirrorImage(Node root)
+    {
+        if(root==null)
+            return root;
+        else if(root.left==null && root.right==null)
+            return root;
+        Node temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+        root.left=mirrorImage(root.left);
+        root.right=mirrorImage(root.right);
+        return root;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class Main {
     public static void main(String args[])
     {
         Node root=null;
         BST treeNode=new BST();
-        root=treeNode.createBST(root,9);
-        root=treeNode.createBST(root,5);
-        root=treeNode.createBST(root,90);
+        root=treeNode.createBST(root,45);
+        root=treeNode.createBST(root,39);
+        root=treeNode.createBST(root,78);
+        root=treeNode.createBST(root,54);
+        root=treeNode.createBST(root,79);
+        root=treeNode.createBST(root,55);
+        root=treeNode.createBST(root,80);
+        System.out.println(treeNode.height(root));
+        System.out.println(treeNode.totalNode(root));
+        System.out.println(treeNode.internalNode(root));
+        System.out.println(treeNode.externalNode(root));
+        root=treeNode.mirrorImage(root);
         treeNode.preOrderTraversal(root);
-        System.out.println("Rashindra");
+
     }
 }
